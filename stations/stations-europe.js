011ca379c1dec8ab9d8238397a3dc53597622d21
Escort95 <31548328+Escort95@users.noreply.github.com>
2018-08-01 22:01:35 +0200

    Add files via upload

---
+++ stations/stations-europe.js
@@ -0,0 +1,253 @@
+    "austria": [
+        {
+            name: "ORF Ö1",
+            logo: "stations/images-europe/austria/ORF Ö1.png",
+            url: "http://mp3stream3.apasf.apa.at/;stream.mp3"
+        },
+        {
+            name: "ORF Ö3",
+            logo: "stations/images-europe/austria/ORF Ö3.png",
+            url: "http://mp3stream7.apasf.apa.at/;stream.mp3"
+        },
+        {
+            name: "ORF FM4",
+            logo: "stations/images-europe/austria/ORF FM4.png",
+            url: "http://mp3stream1.apasf.apa.at/;stream.mp3"
+        },
+        {
+            name: "KroneHit",
+            logo: "stations/images-europe/austria/KroneHit.png",
+            url: "http://onair-ha1.krone.at/kronehit-hp.mp3"
+        },
+        {
+            name: "Life Radio",
+            logo: "stations/images-europe/austria/Life Radio.jpg",
+            url: "http://stream1.liferadio.at/"
+        },
+        {
+            name: "Radio 88.6",
+            logo: "stations/images-europe/austria/Radio 88.6.png",
+            url: "http://xapp2450489345c1000321.f.l.i.lb.core-cdn.net/1000321copo/live/app2450489345/w2445344770/live_de_96.mp3"
+        },
+    ],
+    "austria-wien": [
+	    {
+            name: "ORF Ö2 (Wien)",
+            logo: "stations/images-europe/austria/ORF Ö2.png",
+            url: "http://mp3stream2.apasf.apa.at/;stream.mp3"
+        },
+    ],
+    "austria-salz": [
+        {
+            name: "Welle 1 (Salzburg)",
+            logo: "stations/images-europe/austria/Welle 1.png",
+            url: "http://live.welle1.at:8128/;"
+        },
+    ],
+    "poland": [
+        {
+            name: "PR Jedynka",
+            logo: "stations/images-europe/poland/Polskie Radio 1.jpg",
+            url: "http://stream3.polskieradio.pl:8900/;"
+        },
+        {
+            name: "PR Dwójka",
+            logo: "stations/images-europe/poland/Polskie Radio 2.jpg",
+            url: "http://stream3.polskieradio.pl:8902/;"
+        },
+        {
+            name: "PR Trójka",
+            logo: "stations/images-europe/poland/Polskie Radio 3.jpg",
+            url: "http://stream3.polskieradio.pl:8904/;"
+        },
+        {
+            name: "PR  24",
+            logo: "stations/images-europe/poland/Polskie Radio 24.jpg",
+            url: "http://stream3.polskieradio.pl:8080/;"
+        },
+        {
+            name: "R-MARYJA",
+            logo: "stations/images-europe/poland/Radio Maryja.png",
+            url: "https://cors-anywhere.herokuapp.com/http://94.42.167.5:1935/live/rm.sdp/playlist.m3u8"
+        },
+        {
+            name: " PLUS ",
+            logo: "stations/images-europe/poland/Radio Plus.jpg",
+            url: "http://www.radio-internetowe.com/eskago.php?source=http://www.eskago.pl/radio/radio-plus"
+        },
+        {
+            name: "RadioZET",
+            logo: "stations/images-europe/poland/Radio Zet.svg",
+            url: "http://redir.atmcdn.pl/sc/o2/Eurozet/live/audio.livx?audio=5"
+        },
+        {
+            name: "RMF FM",
+            logo: "stations/images-europe/poland/RMF FM.svg",
+            url: "http://31.192.216.10/RMFFM48"
+        },
+        {
+            name: "RMF Classic",
+            logo: "stations/images-europe/poland/RMF Classic.png",
+            url: "http://31.192.216.8/RMFCLASSIC48"
+        },
+    ],
+	"poland-eska": [
+        {
+            name: " ESKA ",
+            logo: "stations/images-europe/poland/Radio Eska.png",
+            url: "http://85.219.133.18/radio.php?id=1"
+        }
+    ],
+	"poland-max": [
+        {
+            name: "RMFMAXXX",
+            logo: "stations/images-europe/poland/RMF MAXXX.png",
+            url: "http://31.192.216.8:8000/rmf_maxxx"
+        }
+    ],
+    "poland-wrzes": [
+        {
+            name: "Września",
+            logo: "stations/images-europe/poland-poznan/wrzes.jpg",
+            url: "http://s1.slotex.pl:7446/;stream.mp3"
+        }
+    ],
+	"poland-wawa": [
+	    {
+            name: " WAWA ",
+            logo: "stations/images-europe/poland/Radio WAWA.png",
+            url: "http://www.radio-internetowe.com/eskago.php?source=http://www.eskago.pl/radio/wawa"
+        }
+    ],
+    "poland-poznan": [
+        {
+            name: "Polskie Radio Poznań",
+            logo: "stations/images-europe/poland-poznan/poznan.jpg",
+            url: "http://lodz.radio.pionier.net.pl:8000/pl/merkury.mp3"
+        }
+    ],
+    "poland-byd": [
+        {
+            name: "Weekend FM",
+            logo: "stations/images-europe/poland-byd/naklo.png",
+            url: "http://stream.weekendfm.pl:8000/weekendfm_najlepsza.mp3"
+        }
+    ],
+    "poland-lodz": [
+        {
+            name: "Parada",
+            logo: "stations/images-europe/poland-lodz/parada.png",
+            url: "http://stream4.nadaje.com:15274/live"
+        }
+    ],
+    "poland-slask": [
+        {
+            name: "Express",
+            logo: "stations/images-europe/poland-slask/express.png",
+            url: "http://stream4.nadaje.com:13324/express"
+        },
+        {
+        	name: "PR Katowice",
+        	logo: "stations/images-europe/poland-slask/kato.png",
+        	url: "http://stream4.nadaje.com:9212/radiokatowice"
+        }
+    ],
+    "poland-gdansk": [
+        {
+            name: "Kaszebe",
+            logo: "stations/images-europe/poland-gdansk/kaszebe.jpg",
+            url: "http://stream3.nadaje.com:8048/;"
+        }
+    ],
+    "poland-zg": [
+        {
+            name: "Muzyczne Radio",
+            logo: "stations/images-europe/poland-wro/muzyczne.png",
+            url: "http://stream.nadaje.com:9000/;stream"
+        }
+
+    ],
+    "poland-wro": [
+        {
+        	name: "RADIOGRA",
+        	logo: "stations/images-europe/poland-wro/gra.jpg",
+        	url: "http://217.74.72.10:8000/radio_gra_wroc"
+        }
+    ],
+    "poland-pogo": [
+        {
+            name: "Pogoda",
+            logo: "stations/images-europe/poland-war/pogoda.png",
+            url: "http://stream10.radioagora.pl/tuba140-1.mp3"
+        }
+    ],
+    "poland-war": [
+        {
+            name: "POP Radio",
+            logo: "stations/images-europe/poland-war/popradio.png",
+            url: "http://stream.radio7.pl:9000/;stream"
+        }
+    ],
+    "poland-ols": [
+        {
+            name: "PR Olsztyn",
+            logo: "stations/images-europe/poland-ols/ols.jpg",
+            url: "http://213.73.25.178:7066/;stream"
+        }
+    ],
+    "poland-voxfm": [
+            {
+            name: "VOX FM",
+            logo: "stations/images-europe/poland/Radio VOX FM.png",
+            url: "http://85.219.133.18/radio.php?id=4"
+        },
+    ],
+    "poland-ar": [
+        {
+            name: "Antyradio",
+            logo: "stations/images-europe/poland/Antyradio.png",
+            url: "http://ant-waw-01.cdn.eurozet.pl:8602/;"
+        }
+    ],
+    "poland-zlote": [
+        {
+            name: "Złote Przeboje",
+            logo: "stations/images-europe/poland/Radio Złote Przeboje.png",
+            url: "http://lodz.radio.pionier.net.pl:8000/pl/tuba9-1.mp3"
+        },
+    ],
+    "poland-lub": [
+        {
+            name: "Radio Freee",
+            logo: "stations/images-europe/poland-lub/freee.png",
+            url: "http://94.230.19.202:8000/radiofreee"
+        },
+    ],
+    "poland-bb": [
+        {
+            name: "Radio Bielsko",
+            logo: "stations/images-europe/poland-bb/bb.jpg",
+            url: "http://stream4.nadaje.com:13322/radiobielsko"
+        },
+    ],  
+    "poland-suw": [
+        {
+            name: "Radio 5",
+            logo: "stations/images-europe/poland-suw/5.jpg",
+            url: "http://51.254.35.19:8035/listen.mp3"
+        },
+    ],
+	"poland-tok": [
+	    {
+            name: "TOK FM",
+            logo: "stations/images-europe/poland/Tok FM.png",
+            url: "http://wroclaw.radio.pionier.net.pl:8000/pl/tuba10-1.mp3"
+        },
+    ],
+    "poland-nak": [
+	    {
+            name: "Radio Nakło",
+            logo: "stations/images-europe/poland-nak/naklo.jpg",
+            url: "http://sluchaj.107i5.fm/;"
+        },
+    ],
